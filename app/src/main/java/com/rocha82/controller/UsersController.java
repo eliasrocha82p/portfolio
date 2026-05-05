003480e49a464e6d5b5b3a12950bf5a1ef9bd23b
Elias <eliasrocha82p@gmail.com>
2026-05-05 05:02:13 -0300

    service add

--- app/src/main/java/com/rocha82/controller/UsersController.java
+++ app/src/main/java/com/rocha82/controller/UsersController.java
@@ -2,12 +2,14 @@ package com.rocha82.controller;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import com.rocha82.dto.UserDTO;;
+import com.rocha82.service.UserService;
+import com.rocha82.dto.UserDTO;
+
 @RestController
 public class UsersController{
     @GetMapping("/user/{id}")
     public String get(){
-        UserDTO userDTO = new UserDTO();
-        return userDTO.getByID(10);
+        UserService userService = new UserService();
+        return userService.get(23,"Mel Soares").toString();
     }
 }
--- daos/src/main/java/com/rocha82/User.java
+++
@@ -1,6 +0,0 @@
-package com.rocha82.dao;
-public class User{
-    public String getByID(int id){
-        return "Object from Repository";
-    }
-}
